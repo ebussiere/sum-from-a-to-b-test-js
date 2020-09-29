bc2937c679d9dea5aa9b55752708de8e91c0ca45
ebussiere <eddy@embtech.ca>
2020-09-29 19:30:14 +0000

    linted

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-function sum(fromN, toN) {
+const sum = (fromN, toN) => {
   let res = fromN;
-  if (fromN == toN) {
+  if (fromN === toN) {
     return res;
   }
   fromN++;
   return res + sum(fromN, toN);
-}
+};
 module.exports = sum;
 
